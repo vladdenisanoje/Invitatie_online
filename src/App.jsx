4cb2ba1653a71d2f35f5d57142bdbbcc021aa86c
vladdenisanoje <vlad.denisa.noje@gmail.com>
2025-10-24 02:35:16 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,35 @@ import InfoPage from './pages/InfoPage';
 import BottomNav from './components/BottomNav';
 import ToastContainer from './components/ToastContainer';
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/camera" element={<CameraPage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/info" element={<InfoPage />} />
+        </Routes>
+        
+        <BottomNav />
+        <ToastContainer />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+
+
+/*import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CameraPage from './pages/CameraPage';
+import GalleryPage from './pages/GalleryPage';
+import InfoPage from './pages/InfoPage';
+import BottomNav from './components/BottomNav';
+import ToastContainer from './components/ToastContainer';
+
 
 export default function App() {
   return (
@@ -26,4 +55,4 @@ export default function App() {
       </div>
     </BrowserRouter>
   );
-}
+}*/
