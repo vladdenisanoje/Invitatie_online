78f7aa2f03b7700790825b69721735686067aaec
vladdenisanoje <vlad.denisa.noje@gmail.com>
2025-10-24 02:17:42 +0300

    Create Toast.jsx

---
+++ src/components/Toast.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+
+export default function Toast({ message, type, onClose }) {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      onClose();
+    }, 3000);
+    
+    return () => clearTimeout(timer);
+  }, [onClose]);
+
+  return (
+    <div className={`toast toast-${type}`}>
+      <span>{message}</span>
+    </div>
+  );
+}
