3b4cb310a5be277084fa2e01111855be08dca030
vladdenisanoje <vlad.denisa.noje@gmail.com>
2025-10-24 17:56:29 +0300

    Create imgbbAlbum.js

---
+++ src/utils/imgbbAlbum.js
@@ -0,0 +1,11 @@
+export async function fetchImgBBAlbumImages(albumUrl) {
+  const res = await fetch(albumUrl);
+  const html = await res.text();
+  const imgUrls = [];
+  const regex = /<img[^>]+src="(https:\/\/i\.ibb\.co\/[^"]+)"/g;
+  let match;
+  while ((match = regex.exec(html)) !== null) {
+    imgUrls.push(match[1]);
+  }
+  return imgUrls;
+}
