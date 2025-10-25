20a7caf69db16892cf7d573b21109da073476272
vladdenisanoje <vlad.denisa.noje@gmail.com>
2025-10-25 21:46:20 +0300

    Update imgbbAlbum.js

--- src/utils/imgbbAlbum.js
+++ src/utils/imgbbAlbum.js
@@ -1,12 +1,19 @@
 // src/utils/imgbbAlbum.js
 export async function fetchImgBBAlbumImages(albumUrl) {
-  const res = await fetch(albumUrl);
-  const html = await res.text();
-  const imgUrls = [];
-  const regex = /<img[^>]+src="(https:\/\/i\.ibb\.co\/[^"]+)"/g;
-  let match;
-  while ((match = regex.exec(html)) !== null) {
-    imgUrls.push(match[1]);
+  try {
+    const res = await fetch(albumUrl);
+    const html = await res.text();
+    const imgUrls = [];
+    // Regex pentru a extrage toate imaginile i.ibb.co din HTML
+    const regex = /<img[^>]+src="(https:\/\/i\.ibb\.co\/[^"]+)"/g;
+    let match;
+    while ((match = regex.exec(html)) !== null) {
+      imgUrls.push(match[1]);
+    }
+    // Elimină duplicate
+    return [...new Set(imgUrls)];
+  } catch (error) {
+    console.error('Eroare la încărcarea albumului ImgBB:', error);
+    return [];
   }
-  return [...new Set(imgUrls)];
 }
